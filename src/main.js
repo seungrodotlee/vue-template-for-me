cec5328a3b75cd93e6b11a2b209c011055a5ff03
seungrodotlee <srlee0226w@gmail.com>
2021-08-13 10:26:23 +0900

    init

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
+import Vue from "vue";
+import Buefy from "buefy";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import "./styles/main.css";
+import "buefy/dist/buefy.css";
+
+Vue.config.productionTip = false;
+Vue.use(Buefy);
 
 new Vue({
   router,
   store,
-  render: h => h(App)
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
